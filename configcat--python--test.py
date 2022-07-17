82eb91f83fc5107e789c0f28b52176476edaafbc
grandibra <asgerliibrahim@gmail.com>
2022-07-17 21:04:04 +0400

    Create simple function to test some toggle features

--- configcat--python--test.py
+++ configcat--python--test.py
@@ -1,23 +1,27 @@
 from time import sleep
-
+from configcatclient.user import User
 import configcatclient
 
-configcat_client = configcatclient.create_client('7GTaCIDR7U-PvHan9Okx4w/Lw4lR4Uxa0OelhtJx88RZA')
-
-
-myfeatureflag = configcat_client.get_value('myfeatureflag', False)
-new_feature = configcat_client.get_value('new_feature', False)
-
-print('myfeatureflag\'s value from ConfigCat: ' + str(myfeatureflag))
-print('new_feature\'s value from ConfigCat: ' + str(new_feature))
-
-green = '\033[3fwf4e'
+green = '\033[32m'
 red = '\033[31m'
 
-while True:
-    feature = configcat_client.get_value('myfeatureflag',False)
+
+def testfunction(userobject1, userobject2):
+    configcat_client = configcatclient.create_client('7GTaCIDR7U-PvHan9Okx4w/Lw4lR4Uxa0OelhtJx88RZA')
+    feature = configcat_client.get_value('myfeatureflag', False, userobject1)
+    new_feature = configcat_client.get_value('new_feature', False, userobject2)
+    print('myfeatureflag\'s value from ConfigCat: ' + str(feature))
+    print('new_feature\'s value from ConfigCat: ' + str(new_feature))
+
     if feature:
         print('\r{}feature[x]'.format(green), end='')
     else:
         print('\r{}feature[o]'.format(red), end='')
     sleep(1)
+
+
+country = input('where are you from?')
+age = int(input('how old are you?'))
+userobject1 = (User('country', country=country))
+userobject2 = (User('age', custom={'age':age}))
+testfunction(userobject1, userobject2)
